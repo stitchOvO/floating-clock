1dc7786af574f94063a55a7303750125320fd9bc
zzh <132432@qq.com>
2026-04-03 16:24:39 +0800

    Update all dependencies and GitHub Actions configuration for successful build

--- app/src/main/java/com/example/floatingclock/MainActivity.java
+++ app/src/main/java/com/example/floatingclock/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
