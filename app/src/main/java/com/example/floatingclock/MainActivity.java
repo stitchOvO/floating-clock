b5c012a53f7e188f36cfd8b429b4f347b5d351ee
zzh <132432@qq.com>
2026-04-03 08:53:50 +0800

    Update all dependencies to be compatible with Java 8

--- app/src/main/java/com/example/floatingclock/MainActivity.java
+++ app/src/main/java/com/example/floatingclock/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
